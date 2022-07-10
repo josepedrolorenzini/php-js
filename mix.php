c856d21aa7b493eadfa32077c582fced42687c47
jplorenzini <51427975+josepedrolorenzini@users.noreply.github.com>
2022-07-10 17:17:41 +1000

    Update mix.php

--- mix.php
+++ mix.php
@@ -1,23 +1,45 @@
 <?php
- 
- $callmelater = function ($val , $i){
-    return  $val + $i; 
-};
 
-    $multiply = function ($x, $y) {
-        return $x * $y;
-};
+use function Matrix\multiply;
+
+if(isset($_POST["submit"])){
+
+    $callmelater = function ($val , $i){
+        return  $val + $i; 
+    };
+    
+        $multiply = function ($x, $y) {
+            return $x * $y;
+    };
+            $number1 = $_POST['number1'];
+            $number2 = $_POST['number2'];
+            echo $number1 + $number2;
+            echo multiply($number1,$number2);
+
+}
 
 
 ?>
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>" onclick="xhsgsh()">click</a>
+<!-- <a href="<?php //echo $_SERVER['PHP_SELF']; ?>" onclick="xhsgsh(x,y)">click</a>
 
 <script>
-<?php $result = $multiply(1,2); ?>
-function xhsgsh(){
-    console.log(<?php echo $result; ?>);
+<?php //$result = $multiply(1,2); ?>
+function xhsgsh(x,y){
+    console.log(<?php //echo $result; ?>);
+    console.log(x + y );
 }
 
-xhsgsh();
+xhsgsh(2,3);
+</script> -->
+<br>
+<br>
+<br>
+<br>
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<input type="number" name="number1" value="number1" placeholder="number1"><br>
+<input type="number" name="number2" value="number2" placeholder="number2"><br>
+<input type="submit" name="submit" value="submit">
+</form>
 
-</script>
+
+<div id="container">nada</div>
